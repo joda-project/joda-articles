58a5415e0aa5c66a9b28dafe34ac261247a4ca5d
Tadej Novak <tadej@tano.si>
2016-12-29 20:47:23 +0100

    Expose version and articles as a feature

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 /* jshint node: true */
 'use strict';
 
+var getVersion = require('git-repo-version');
+
 module.exports = {
-  name: 'joda-articles'
+  name: 'joda-articles',
+  config: function() {
+    let joda = {
+      features: {
+        articles: true
+      },
+      versions: {}
+    };
+
+    let path = this.project.root;
+    if (this.project.pkg.name !== 'joda-articles') {
+      path = this.project.addonPackages['joda-articles'].path;
+    }
+
+    var version = getVersion(null, path);
+    if (version) {
+      joda.versions['joda-articles'] = version;
+    }
+
+    return {
+      Joda: joda
+    };
+  },
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
